6d530c43df50b7906d2193aac8eb44220f0b61bb
Bhargavi <bhargavijavidi@gmail.com>
2022-07-06 00:18:36 +0530

    first commit

--- app/src/main/java/com/example/sandsyndicate/Authentication/Register.kt
+++ app/src/main/java/com/example/sandsyndicate/Authentication/Register.kt
@@ -3,8 +3,10 @@ package com.example.sandsyndicate.Authentication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sandsyndicate.R
+import com.example.sandsyndicate.databinding.ActivityLoginBinding
 
 class Register : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
