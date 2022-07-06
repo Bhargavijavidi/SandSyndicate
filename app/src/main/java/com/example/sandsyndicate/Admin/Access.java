861af686a76e4ee0b3b7009f0edacfa059c5d810
Bhargavi <bhargavijavidi@gmail.com>
2022-07-06 12:51:31 +0530

    second commit

---
+++ app/src/main/java/com/example/sandsyndicate/Admin/Access.java
@@ -0,0 +1,16 @@
+package com.example.sandsyndicate.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sandsyndicate.R;
+
+public class Access extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_access);
+    }
+}
---
+++ app/src/main/java/com/example/sandsyndicate/Approver/Approversanddetails.java
@@ -0,0 +1,16 @@
+package com.example.sandsyndicate.Approver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sandsyndicate.R;
+
+public class Approversanddetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_approversanddetails);
+    }
+}
